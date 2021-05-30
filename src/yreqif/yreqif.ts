0d392dfd2daf9b9f65b4bc6ac5d3809aa43d16ab
IonRod <rrod76@gmail.com>
2021-05-30 16:23:54 +0300

    - Added prototype for CRUD operations

--- src/yreqif/yreqif.ts
+++ src/yreqif/yreqif.ts
@@ -1,3 +1,4 @@
+import { Identifiable } from "../reqif-naive/definitions/ReqIFBasicClasses";
 import { ReqIF } from "../reqif-naive/ReqIF";
 
 //--------------------
@@ -18,5 +19,60 @@ export class yReqIF {
         } else {
             this.reqif = new ReqIF();
         }
+    };
+
+    //---------------------
+    //CRUD operations
+    //Assuming that all edits are subclasses of identifiable
+    create<T extends Identifiable>(props: T) {
+        /*TODO:
+        how to implement different create rules for different classes?
+        */
+    }
+
+    read(identifier: string) {
+        if(this.index) {
+            return this.index[identifier];
+        } else {
+            console.error("Missing item in index, identifier: ", identifier);
+        }
+    }
+    
+    update(props: Identifiable) {
+        /*TODO:
+        how to implement different update rules for different classes?
+        */
+    }
+
+    delete(identifier: string | Identifiable) {
+        /*TODO:
+        how to implement different delete rules for different classes?
+        */
     }
 }
+
+
+
+/*
+//
+Ideas:
+1. Everything that could be edited is Identifiable
+
+CRUD operations
+
+1. Create
+2. Read
+3. Update
+4. Delete
+
+Additional operations
+5. Visualisations!
+
+Classes to work with
+1. ReqIf header
+2. DataType
+3. SpecType
+4. SpecObject
+5. Specification
+
+*/

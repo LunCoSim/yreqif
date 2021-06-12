0dd372e1010a2114af09c7b6bd066dfaf3308ae4
IonRod <rrod76@gmail.com>
2021-06-12 23:08:53 +0300

    Sample creation of new objects

--- tests/test_reqif.ts
+++ tests/test_reqif.ts
@@ -25,12 +25,30 @@ npm install -g fast-xml-parser
 
 //Should create every Datatype
 
+import { Specification } from "../src/reqif-naive/content/ReqIFSpecification";
+import { SpecObject } from "../src/reqif-naive/content/ReqIFSpecObject";
 import { DatatypeDefinitionInteger } from "../src/reqif-naive/definitions/ReqIFDatatypeDefinition";
+import { SpecificationType } from "../src/reqif-naive/definitions/ReqIFSpecTypes";
 import { yReqIF } from "../src/yreqif/yreqif";
 
 const yreqif = new yReqIF();
+
 const dt_integer = new DatatypeDefinitionInteger();
+const specObject = new SpecObject();
+
+const specificationType = new SpecificationType();
+const specification = new Specification();
 
 yreqif.create(dt_integer);
-console.log(JSON.stringify(dt_integer, null, 4));
+yreqif.create(specificationType);
+yreqif.create(specObject);
+yreqif.create(specification);
+
+
+function toJSON(obj: object) {
+    return JSON.stringify(obj, null, 4)
+}
+// console.log();
+// console.log(JSON.stringify(specificationType, null, 4));
+
 console.log(yreqif.to_JSON());

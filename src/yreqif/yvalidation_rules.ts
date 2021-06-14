2c0658e2cb92d0e0e4a9ec260d97b629005dbb67
IonRod <rrod76@gmail.com>
2021-06-14 19:15:46 +0300

    - Validation rule: can't delete header

--- src/yreqif/yvalidation_rules.ts
+++ src/yreqif/yvalidation_rules.ts
@@ -4,11 +4,18 @@ update
 delete
 */
 
+import { ReqIFHeader } from "../reqif-naive/ReqIFHeader";
 import { yReqIF } from "./yreqif";
 
 export function canDelete(yreqif: yReqIF, action: any): boolean {
     //TODO: Rule that can't delete header!
 
+    let obj: unknown = yreqif.getObject(action.identifier);
+
+    //Rule: Can't remove header
+    if(obj instanceof ReqIFHeader) {
+        return false;
+    }
     return true; //true by default
     //check cases  what would happen if deleted. Any backlincks?
     //Suggest merging?

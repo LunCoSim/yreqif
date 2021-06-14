8aa6cd6d39e52325458c5d85ee15908cc34fdb25
IonRod <rrod76@gmail.com>
2021-06-14 16:47:40 +0300

    - Setting default values for header

--- src/reqif-naive/basic/ReqIFBasicClasses.ts
+++ src/reqif-naive/basic/ReqIFBasicClasses.ts
@@ -3,8 +3,17 @@
 
 import { v4 as uuidv4 } from 'uuid';
 
+export interface IIdentifiable {
+    desc?: string; //[0..1]
+    identifier: string; //UUID
+    lastChange?: Date;
+    longName?: string; //[0..1];
+
+    alternativeID?: AlternativeID; //Actually
+}
+
 //TODO How to connect Identifiable and AlternativeID???? check page 29 figure 10.2 from ReqIF spec
-export class Identifiable {
+export class Identifiable implements IIdentifiable {
     desc?: string; //[0..1]
     identifier: string; //UUID
     lastChange?: Date;
@@ -12,7 +21,7 @@ export class Identifiable {
 
     alternativeID?: AlternativeID; //Actually
 
-    constructor(props?:Identifiable) {
+    constructor(props?:IIdentifiable) {
         if(props) {
             this.identifier = props['identifier'] || uuidv4() as string;
             this.desc = props['desc'];
